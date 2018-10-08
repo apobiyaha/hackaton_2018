cc74cef8641ad6eaec6e436b21c6d8c8d1c3553d
apobiyaha <apobiyaha@intervale.ru>
2018-10-08 14:33:12 +0300

    myCards

---
+++ src/components/card/Card.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="card" :class="{'card-active': isActive }">
+    <div class="row justify-end">
+      <div :class="['brand-base', brand]">
+      </div>
+    </div>
+    <div class="row number">
+      {{number}}
+    </div>
+    <div class="row number justify-around">
+      <div class="name">
+        {{name}}
+      </div>
+      <div>
+        <q-btn
+              round
+              color="amber"
+        >
+         <span style="font-size: 0.6rem">{{balance}} {{currency}}</span>
+        </q-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'card',
+  props: {
+    brand: {
+      default: 'visa'
+    },
+    number: {
+      default: '4111 1111 1111 1111'
+    },
+    isActive: {
+      default: false
+    },
+    name: {
+      default: 'NoName'
+    },
+    balance: {
+      default: '0'
+    },
+    currency: {
+      default: 'RUB'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .card {
+    margin: 10px;
+    border-radius: 10px;
+    width: 250px;
+    height: 150px;
+    background-color: #f1f1f1;
+  }
+  .card-active {
+    border: limegreen solid 1px;
+  }
+  .brand-base {
+    display: inline-block;
+    width: 50px;
+    height: 32px;
+    margin-top: 10px;
+    margin-right: 10px;
+    background-size: contain;
+  }
+  .visa {
+    background-image: url('../../assets/img/vendors/visa.svg');
+  }
+  .maestro{
+    background-image: url('../../assets/img/vendors/maestro.svg');
+  }
+  .mastercard{
+    background-image: url('../../assets/img/vendors/mastercard.svg');
+  }
+  .number {
+    color: gray;
+    margin-left: 30px;
+    margin-top: 15px;
+    font-weight: bold;
+    font-size: 1.1rem;
+  }
+  .name {
+    font-size: 0.8rem;
+  }
+</style>
--- src/pages/Cards.vue
+++ src/pages/Cards.vue
@@ -1,11 +1,70 @@
 <template>
   <q-page>
-    cards
+    <div class="row justify-center" v-for="card in cards" :key="card">
+      <card  :brand="card.brand"
+             :number="card.number"
+             :is-active="card.active"
+             :name="card.name"
+             :balance="card.balance"
+             :currency="card.currency"
+             @click="setActive(card)">
+      </card>
+    </div>
+    <div class="row justify-center">
+      <q-btn
+        size="1.1rem"
+        color="primary"
+        icon="add_circle_outline"
+        @click="countShots">
+        <span style="font-size: 0.6rem">Add card</span>
+      </q-btn>
+    </div>
   </q-page>
 </template>
 
 <script>
+import Card from '../components/card/Card'
 export default {
-  name: 'Cards'
+  name: 'Cards',
+  components: { Card },
+  data () {
+    return {
+      cards: {
+        1: {
+          brand: 'mastercard',
+          number: '5555 5555 5555 4444',
+          name: 'my salary card',
+          currency: 'RUB',
+          balance: '123',
+          active: true
+        },
+        2: {
+          brand: 'maestro',
+          number: '6011 1111 1111 1117',
+          name: 'my second card',
+          currency: 'USD',
+          balance: '12',
+          active: false
+        },
+        3: {
+          brand: 'visa',
+          number: '4111 1111 1111 1111',
+          name: 'top secret card',
+          currency: 'EUR',
+          balance: '111',
+          active: false
+        }
+      }
+    }
+  },
+  methods: {
+    // setActive (card) {
+    //   this.cards.card.active = true;
+    // }
+  }
 }
 </script>
+
+<style scoped>
+
+</style>
