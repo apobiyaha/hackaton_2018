a5e29d50d0897e3f3cf96f29afb618bc8b9d9ac7
apobiyaha <apobiyaha@intervale.ru>
2018-10-10 16:47:51 +0300

    stat

--- src/components/statistics/Analize.vue
+++ src/components/statistics/Analize.vue
@@ -1,6 +1,11 @@
 <template>
 <div>
-  <apexcharts width="400" type="bar" :options="chartOptions" :series="series"></apexcharts>
+  <div class="row">
+    <apexcharts width="400" type="bar" :options="chartOptions" :series="series"></apexcharts>
+  </div>
+  <div class="row">
+    <apexcharts width="400" type="pie" :options="chartOptions" :series="seriesPie" :labels="labels"></apexcharts>
+  </div>
 </div>
 </template>
 
@@ -19,12 +24,14 @@ export default {
         },
         xaxis: {
           categories: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        }
+        },
+        labels: ['Штрафы', 'Развлечения', 'Коммунальные', 'Одежда', 'Еда']
       },
       series: [{
         name: 'Расходы по месяцам',
         data: [30, 40, 42, 43, 42, 55, 65, 71]
-      }]
+      }],
+      seriesPie: [1, 2, 3, 4, 5]
     }
   }
 }
