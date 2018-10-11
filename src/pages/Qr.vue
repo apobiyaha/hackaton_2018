65e991d1e9928628cc8cf408f1356ae42e14f95d
ashapovalov <ashapovalov@intervale.ru>
2018-10-11 19:58:21 +0300

    ashapovalov

---
+++ src/pages/Qr.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page>
+    <img src="../assets/img/Qr.png" height="100%" width="100%" alt="qr">
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Qr'
+}
+</script>
+
+<style scoped>
+
+</style>
